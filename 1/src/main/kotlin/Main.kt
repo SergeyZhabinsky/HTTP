4a14de198597b275a81186049099f4cbf05cb670
SergeyZhabinsky <szhab@list.ru>
2022-02-18 15:50:41 +0300

    Изменённый код от 18.02.2022

--- 1/src/main/kotlin/Main.kt
+++ 1/src/main/kotlin/Main.kt
@@ -18,35 +18,25 @@ class Homo{
 
 fun main(args: Array<String>) {
     val st = Start();
+
     st.getArray()
-/*
-    var h = Homo()
-    h.age = 15
-    println("Hello World! " + h.age)
-    h.age = 16
-    println("Hello World! " + h.age)
-    h.age = 17
-    println("Hello World! " + h.age)
-    h.age = 18
-    println("Hello World! " + h.age)
-    h.age = 19
-    println("Hello World! " + h.age)
-    h.age = 20
-    println("Hello World! " + h.age)
-    h.age = 21
-    println("Hello World! " + h.age)
 
-    var w1 = Weather("Moscow", -7)
-    var w2 = Weather("London")
-    var w3 = Weather( temperature = 2)
-    println(w1.town + " = " + w1.temperature)
-    println(w2.town + " = " + w2.temperature)
-    println(w3.town + " = " + w3.temperature)
+    if( st.setValue( 0,"floorCount", "55" ) )
+        println("OK OK OK")
+    else
+        println("NO NO NO")
+    if( st.setValue( 0,"colour", "BROWN" ) )
+        println("OK OK OK")
+    else
+        println("NO NO NO")
+    if( st.setValue( 2,"floorCount", "55" ) )
+        println("OK OK OK")
+    else
+        println("NO NO NO")
+    if( st.setValue( 3,"floorCount", "55" ) )
+        println("OK OK OK")
+    else
+        println("NO NO NO")
 
-    for(i in 1 .. 6){
-        println("O")
-    }
-*/
-    // Try adding program arguments at Run/Debug configuration
-//    println("Program arguments: ${args.joinToString()}")
+    st.getArray()
 }
--- 1/src/main/kotlin/data/ClHome.kt
+++ 1/src/main/kotlin/data/ClHome.kt
@@ -1,5 +1,7 @@
 package com.zhabinsky.firstlesson.data
 
+import kotlin.reflect.typeOf
+
 data class ClHome( val floorCount : Int = 2, val colour : String = "Yellow"  )
 //data class Weather(var town : String = "ANY", var temperature : Int = 10)
 
@@ -8,18 +10,33 @@ class Start() {
     var homes : ArrayList<ClHome> = arrayListOf()
 
     init {
-//        var homes : ArrayList<ClHome> = arrayListOf(ClHome())
         homes.add( ClHome(floorCount = 3, "White") )
         homes.add( ClHome(floorCount = 4, "Yellow") )
         homes.add( ClHome(floorCount = 7, "Green") )
     }
 
 
-    fun getArray(){
-        for(x in homes)
-            println( "Значение = " + x.floorCount + "(" + x.colour + ")")
 
-        homes[1] = homes[1].copy( floorCount = 10 )
+
+    fun setValue( _nClass : Int, _field : String, _value : String ) : Boolean{
+        var bRetValue = true
+
+        if(_nClass.toInt() <= homes.size-1){
+
+            if(_field.equals("floorCount", true) ){
+                homes[_nClass] = homes[_nClass].copy( floorCount = _value.toInt() )
+            }else if(_field.equals("colour", true) ){
+                homes[_nClass] = homes[_nClass].copy( colour = _value )
+            }else bRetValue = false
+
+        }
+        else bRetValue = false
+
+        return bRetValue
+    }
+
+
+    fun getArray(){
         for(x in homes)
             println( "Значение = " + x.floorCount + "(" + x.colour + ")")
     }
